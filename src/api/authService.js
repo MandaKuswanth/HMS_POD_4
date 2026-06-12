fed4d0baba43053ab943d9114e6ada16935142bc
yaswanth-308298 <308298@ust.com>
2026-06-12 21:00:22 +0530

    Deleted SRC

--- src/api/authService.js
+++
@@ -1,4 +0,0 @@
-import api from "../utils/api";
-export const unwrap=(response)=>response?.data?.data||response?.data||response;
-export const registerPatient=async(data)=>unwrap(await api.post("/patient-auth/register",data));
-export const loginPatient=async(data)=>unwrap(await api.post("/patient-auth/login",data));
--- src/components/AppContainer.js
+++
@@ -1,1 +0,0 @@
-import React from"react";import{View,StyleSheet}from"react-native";import COLORS from"../utils/colors";export default function AppContainer({children,style}){return <View style={[s.container,style]}>{children}</View>}const s=StyleSheet.create({container:{flex:1,backgroundColor:COLORS.background}});
