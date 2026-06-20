c6fe8d59dee4e21310be03025c06cb0fc564b6ba
Kuswanth <308324@ust.com>
2026-06-20 06:04:54 +0530

    upate import

--- Angular/src/app/features/appointments/appointment-list/appointment-list.ts
+++ Angular/src/app/features/appointments/appointment-list/appointment-list.ts
@@ -16,6 +16,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ToastrService } from 'ngx-toastr';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 import { Navbar } from '../../../shared/components/navbar/navbar';
 import { Sidebar } from '../../../shared/components/sidebar/sidebar';
