eabc8595813d42c83dd40b1fbebd32d2097526d1
Prasant-Kumar01 <308240@ust.com>
2026-06-23 19:07:18 +0530

    Initial commit optimization

---
+++ Angular/.angular/cache/21.2.14/FRONTEND/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,52 @@
+import {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-WPCDGEI2.js";
+import "./chunk-EMZ5E5WN.js";
+import "./chunk-GUGIMSVJ.js";
+import {
+  Dir
+} from "./chunk-6NCM5CP7.js";
+import "./chunk-ZTF5Q4KX.js";
+import "./chunk-U5OOWBJH.js";
+import "./chunk-EXTZ52HX.js";
+import "./chunk-2JL3FCUK.js";
+import "./chunk-XWQQ42E4.js";
+import "./chunk-PJVWDKLX.js";
+export {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
---
+++ Angular/.angular/cache/21.2.14/FRONTEND/vite/deps/@angular_material_tooltip.js
@@ -0,0 +1,43 @@
+import {
+  MatTooltipModule
+} from "./chunk-MSIHOOQL.js";
+import {
+  MAT_TOOLTIP_DEFAULT_OPTIONS,
+  MAT_TOOLTIP_SCROLL_STRATEGY,
+  MatTooltip,
+  SCROLL_THROTTLE_MS,
+  TOOLTIP_PANEL_CLASS,
+  TooltipComponent,
+  getMatTooltipInvalidPositionError
+} from "./chunk-WVR53PFZ.js";
+import "./chunk-FG7TKNX3.js";
+import "./chunk-WPCDGEI2.js";
+import "./chunk-EMZ5E5WN.js";
+import "./chunk-BUSRVSLX.js";
+import "./chunk-HEXMFTMC.js";
+import "./chunk-GWBU7KI5.js";
+import "./chunk-PLJ2QXBA.js";
+import "./chunk-LW6R42U7.js";
+import "./chunk-YJBVXNXF.js";
+import "./chunk-VVBUAADP.js";
+import "./chunk-N4DOILP3.js";
+import "./chunk-GUGIMSVJ.js";
+import "./chunk-6NCM5CP7.js";
+import "./chunk-XD5KVAM2.js";
+import "./chunk-T3OPMRBR.js";
+import "./chunk-ZTF5Q4KX.js";
+import "./chunk-U5OOWBJH.js";
+import "./chunk-EXTZ52HX.js";
+import "./chunk-2JL3FCUK.js";
+import "./chunk-XWQQ42E4.js";
+import "./chunk-PJVWDKLX.js";
+export {
+  MAT_TOOLTIP_DEFAULT_OPTIONS,
+  MAT_TOOLTIP_SCROLL_STRATEGY,
+  MatTooltip,
+  MatTooltipModule,
+  SCROLL_THROTTLE_MS,
+  TOOLTIP_PANEL_CLASS,
+  TooltipComponent,
+  getMatTooltipInvalidPositionError
+};
