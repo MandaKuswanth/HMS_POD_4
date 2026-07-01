db2ca6066678a58bf3f9fee76f7a36f1dd4bdd9f
Asmi11404 <308266@ust.com>
2026-07-01 17:48:51 +0530

    css updated for listing data

--- Angular/src/app/features/nodes/node-list/node-list.ts
+++ Angular/src/app/features/nodes/node-list/node-list.ts
@@ -14,6 +14,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { NodeService } from '../../../core/services/node';
 import { NodeDialog } from '../node-dialog/node-dialog';
+import { MainComponent } from '../../../shared/components/maincomponent/maincomponent';
 
 @Component({
   selector: 'app-node-list',
@@ -27,6 +28,7 @@ import { NodeDialog } from '../node-dialog/node-dialog';
     MatIconModule,
     MatCardModule,
     MatTooltipModule,
+    MainComponent,
 
   ],
   templateUrl: './node-list.html',
