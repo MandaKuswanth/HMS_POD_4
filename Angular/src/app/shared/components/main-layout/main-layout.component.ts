42154d9f03fe1a18e78ddb8592dcb8c8d744402b
Prasant-Kumar01 <308240@ust.com>
2026-07-02 17:49:48 +0530

    added responsiveness throughtout

--- Angular/src/app/shared/components/main-layout/main-layout.component.ts
+++ Angular/src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 import { Sidebar } from '../sidebar/sidebar';
 import { Navbar } from '../navbar/navbar';
@@ -17,4 +18,20 @@ import { Navbar } from '../navbar/navbar';
     templateUrl: './main-layout.component.html',
     styleUrls: ['./main-layout.component.css']
 })
-export class MainLayoutComponent { }
+export class MainLayoutComponent {
+    readonly sidebarOpen = signal(false);
+
+    constructor(private readonly router: Router) {
+        this.router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe(() => this.sidebarOpen.set(false));
+    }
+
+    toggleSidebar(): void {
+        this.sidebarOpen.update((v) => !v);
+    }
+
+    closeSidebar(): void {
+        this.sidebarOpen.set(false);
+    }
+}
