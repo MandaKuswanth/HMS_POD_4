4c2a5358bc3e9c50cbc63830e589f0cc2000cc5a
Kuswanth <308324@ust.com>
2026-07-07 09:53:05 +0530

    updated angular  services

--- Angular/src/app/core/services/employee.ts
+++ Angular/src/app/core/services/employee.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 export interface EmployeeRequest {
   name: string;
@@ -26,7 +27,7 @@ export interface EmployeeRequest {
 export class EmployeeService {
   private readonly http = inject(HttpClient);
 
-  private readonly baseUrl = 'http://localhost:3000/api/employees';
+  private readonly baseUrl = environment.apiUrl + '/api/employees';
 
   // Public employee registration
   registerEmployee(data: EmployeeRequest): Observable<any> {
