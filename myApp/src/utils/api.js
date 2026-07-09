6f99f3492e94e62417487ebac7d4549cbe5e8725
yaswanth-308298 <308298@ust.com>
2026-07-09 09:57:39 +0530

    API Change for REACT NATIVE

--- myApp/src/utils/api.js
+++ myApp/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import SecureStorage from "./secureStorage";
 
-export const BASE_URL = "https://10.11.73.69:3000/api";
+export const BASE_URL = "https://13.202.52.104/api";
 
 let accessToken = null;
 
